dcebd7a8595660dd0b9e4ceab8332ca293964404
ekelseya <42783392+ekelseya@users.noreply.github.com>
2020-06-22 17:00:39 -0600

    Created React App
    Used create-react-app to build an SPA. Created my first component (Chart.js) and tested fetching data from The COVID Tracking Project.

---
+++ covid-dashboard/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Chart from "./components/Chart";
+
+function App() {
+  return (
+    <Chart />
+  );
+}
+
+export default App;
---
+++ covid-dashboard/src/components/Chart.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+class Chart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://covidtracking.com/api/v1/states/co/daily.json")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        data: result.data
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, data } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>Success!</div>
+                // <ul>
+                //     {items.map(item => (
+                //         <li key={item.name}>
+                //             {item.name} {item.price}
+                //         </li>
+                //     ))}
+                // </ul>
+            );
+        }
+    }
+}
+
+export default Chart;
